74e018e19ddd4334a71d1c3f3ddeb97cb44f5123
julio gabriel <santos2001martins@gmail.com>
2023-07-15 16:02:00 -0300

    refactor(config): corrigido a declaração de modulos as variaveis de cores STATUS, GENERO, BOTÃO E FAVORITOS não estavam no objeto palette

--- src/mui.d.ts
+++ src/mui.d.ts
@@ -1,6 +1,7 @@
-import "@mui/material/styles/createPalette";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Palette, PaletteOptions } from "@mui/material/styles/createPalette";
 declare module "@mui/material/styles/createPalette" {
-  interface PaletteOptions {
+  interface Palette {
     status: {
       statusAlive?: { background?: string; text?: string };
       statusDead?: { background?: string; text?: string };
@@ -17,4 +18,22 @@ declare module "@mui/material/styles/createPalette" {
     button?:string
     favorites?:string
   }
+
+  interface PaletteOptions {
+    status: {
+      statusAlive?: { background?: string; text?: string };
+      statusDead?: { background?: string; text?: string };
+      statusUnknown?: { background?: string; text?: string };
+    };
+    gender?:{
+      male?:string
+      female?:string
+      genderless?:string
+      unknown?:string
+    }
+
+    button?:string
+    favorites?:string
+  }
 }
+
