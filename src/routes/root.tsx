5ddf71df4a2630c50d4a6cc120d7c17c43afeb9d
julio gabriel <santos2001martins@gmail.com>
2023-07-28 17:57:02 -0300

    feat(): componente filtro em todas as paginas

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,15 @@
-import {AppThemeProvider } from "../contexts/ThemeContext";
+import CssBaseline from "@mui/material/CssBaseline";
+import { FiltrosDrawer } from "../components/filtros/Index";
+import { AppThemeProvider } from "../contexts/ThemeContext";
 import { Outlet } from "react-router-dom";
+import ToogleThemeButton from "../components/toogleTheme";
 
 const Root = () => {
     return (
         <AppThemeProvider>
+            <ToogleThemeButton/>
+            <CssBaseline/>
+            <FiltrosDrawer/>
             <Outlet/>
         </AppThemeProvider>
     );
