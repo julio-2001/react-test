49b0a97e52a722369ae19c45c0bc4a13ee599ac2
julio gabriel <santos2001martins@gmail.com>
2023-07-30 15:55:49 -0300

    fix(config): corrigi a configuracao de variaveis de ambiente anterior
    A alteracao corrigiu o erro de variaveis de ambiente que retornavam undefined e impedia a aplicacao funcionar corretamente

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
- /// <reference types="vite/client" />
 /* eslint-disable quote-props */
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-const { DEV } = import.meta.env
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:DEV?"":"react-test"
+export default defineConfig(ctx => {
+  return {
+    plugins: [react()],
+    base:ctx.mode === "development"? "":"react-test"
+  }
 })
