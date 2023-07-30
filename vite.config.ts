2f1c56d820590d31e834b8e518cf5547d675f9fb
julio gabriel <santos2001martins@gmail.com>
2023-07-30 11:43:18 -0300

    chore(config): ajuste a base para ambientes de desenvolvimento e produção

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+ /// <reference types="vite/client" />
+/* eslint-disable quote-props */
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+const { DEV } = import.meta.env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:DEV?"":"react-test"
 })
